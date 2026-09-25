a54bf4aa38841f6078a2f5a666e9fff5e8014025
Bartlomiej Chmiel <bchmiel@antmicro.com>
2026-09-25 11:21:03 +0200

    [#105356] Set injection rollback order

--- src/FaultInjector/src/EventRollback.h
+++ src/FaultInjector/src/EventRollback.h
@@ -29,13 +29,14 @@ class EventRollback {
     std::unique_ptr<s_vpi_vecval[]> vec_;
 
    public:
+    uint64_t id;
     const Signal* signal = nullptr;
     uint64_t time{};
     int bit_idx{};
     s_vpi_value vpi_value{};
 
     explicit EventRollback(const Signal* signal, uint64_t time, int bit_idx, s_vpi_value* nvalp)
-        : signal{signal}, time{time}, bit_idx{bit_idx}, vpi_value{*nvalp} {
+        : id{getNextId()}, signal{signal}, time{time}, bit_idx{bit_idx}, vpi_value{*nvalp} {
         // IEEE 1800-2023 38.15 vpi_get_value()
         const PLI_INT32 numvals = (signal->vpi_width + 31) >> 5;
         vec_ = std::make_unique<s_vpi_vecval[]>(numvals);
@@ -47,6 +48,7 @@ class EventRollback {
     EventRollback& operator=(EventRollback&) = delete;
     EventRollback(EventRollback&& other) noexcept
         : vec_{std::move(other.vec_)},
+          id{other.id},
           signal{std::exchange(other.signal, nullptr)},
           time{other.time},
           bit_idx{other.bit_idx},
@@ -54,6 +56,7 @@ class EventRollback {
     EventRollback& operator=(EventRollback&& other) noexcept {
         if (this != &other) {
             vec_ = std::move(other.vec_);
+            id = other.id;
             signal = std::exchange(other.signal, nullptr);
             time = other.time;
             bit_idx = other.bit_idx;
@@ -62,14 +65,26 @@ class EventRollback {
         return *this;
     }
 
-    bool operator<(const EventRollback& other) const { return time < other.time; }
+    bool operator<(const EventRollback& other) const {
+        if (time == other.time) {
+            return id < other.id;
+        }
+        return time < other.time;
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const EventRollback& ev) {
-        return os << "{[" << ev.time << "] " << ev.signal->path << "(" << ev.bit_idx << ")}";
+        return os << "{[" << ev.time << "] id=" << ev.id << " " << ev.signal->path << "("
+                  << ev.bit_idx << ")}";
     }
 
     vpiHandle handle() const { return signal->vpi_handle.handle(); }
     std::string_view sig_path() const { return signal->path; }
+
+   private:
+    static uint64_t getNextId() {
+        static uint64_t s_s{0};
+        return s_s++;
+    }
 };
 
 }  // namespace fin
